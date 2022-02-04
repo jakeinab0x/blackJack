e7f4a2e1b8c3241d95aec39f7513c8225f5b131d
jakeinab0x <98402073+jakeinab0x@users.noreply.github.com>
2022-02-04 20:56:27 +0000

    Shuffle function in deck builder.js

--- deck builder.js
+++ deck builder.js
@@ -1,18 +1,36 @@
-const numbers = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"]
-const suits = [" of Hearts", " of Diamonds", " of Clubs", " of Spades"]
-function createDeck(numbers, suits) {
-    var deck = []
-    for (var i in suits) {
-        for (var j in numbers) {
-            deck.push(numbers[j] + suits[i])
-        
-        }
-    } 
-return deck
+var numbers = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"]
+var suits = [" of Hearts", " of Diamonds", " of Clubs", " of Spades"]
 
+function getDeck(numbers, suits) {
+    var deck = [];
+    for (i in suits) {
+        for (j in numbers) {
+            deck.push(numbers[j] + suits[i])
+        }
+    }
+    return deck
 }
 
-myDeck = createDeck(numbers, suits)
- console.log('myDeck', myDeck)
- console.log(deck)
+var deck = getDeck(numbers, suits)
 
+//The Fisher-Yates (aka Knuth) Shuffle.
+
+function shuffle(deck) {
+  let currentIndex = deck.length,  randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [deck[currentIndex], deck[randomIndex]] = [
+      deck[randomIndex], deck[currentIndex]];
+  }
+  return deck;
+}
+
+shuffle(deck)
+console.log(deck[0] + ', ' + deck[1])
